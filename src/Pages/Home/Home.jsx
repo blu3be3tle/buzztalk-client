5eaecfab5436a8d9b58f71d2c2114ad2280880f6
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2024-02-19 23:30:58 +0600

    sidebar added with different options

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import {
+  FaHome,
+  FaList,
+  FaRegNewspaper,
+  FaUser,
+  FaUsers,
+} from 'react-icons/fa';
+import { IoMdSettings } from 'react-icons/io';
+import { NavLink, Outlet } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div>Hello</div>
-  )
-}
+    <div className="flex">
+      {/* sidebar */}
+      <div className="w-58 min-h-screen bg-yellow-400">
+        <ul className="menu p-4">
+          <li className="py-1">
+            <NavLink to="/">
+              <FaHome></FaHome>
+              BuzzTalk
+            </NavLink>
+          </li>
 
-export default Home
+          <>
+            <li>
+              <NavLink to="/profile">
+                <FaUser></FaUser>
+                Profile
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/chats">
+                <FaList></FaList>
+                Chats
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/groups">
+                <FaUsers></FaUsers>
+                Groups
+              </NavLink>
+            </li>
+
+            <li>
+              <NavLink to="/settings">
+                <IoMdSettings></IoMdSettings>
+                Settings
+              </NavLink>
+            </li>
+          </>
+        </ul>
+      </div>
+
+      {/* content */}
+      <div className="flex-1">
+        <Outlet></Outlet>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
