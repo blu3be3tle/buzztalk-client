6bc32fc618342fecac3a2e0a7e16c854d2b9f202
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2024-02-22 00:36:37 +0600

    added text input

--- src/Pages/Chats/Chatbox.jsx
+++ src/Pages/Chats/Chatbox.jsx
@@ -7,12 +7,21 @@ const Chatbox = () => {
             src="https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-512.png"
             className="w-12"
           />
-          <h1 className="font-bold">Bruce Wayne</h1>
+          <h1 className="font-bold m-3">Bruce Wayne</h1>
         </div>
 
         <div>
           <button className="btn">...</button>
         </div>
+      </div>
+      <hr />
+
+      <div className="">
+        <input
+          type="text"
+          placeholder="Type here"
+          className="input input-bordered w-full"
+        />
       </div>
     </div>
   );
