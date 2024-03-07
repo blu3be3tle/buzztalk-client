d372a69d7185ace7065303e1aaaf64ffedddaad2
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2024-03-07 15:51:46 +0600

    typos fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import './index.css';
-import AuthProvider from './providers/AuthProvider';
+import AuthProvider from './Providers/AuthProvider';
 import router from './Routes/Routes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
